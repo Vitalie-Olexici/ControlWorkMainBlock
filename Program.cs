a3f85c2b1f925db102f719bcb38796631b54a4a6
Vitalie-Olexici <olexici.dev@gmail.com>
2024-09-11 12:14:08 +0300

    Первый проход по исходному массиву для подсчета строк с длиной <= 3 символов

--- Program.cs
+++ Program.cs
@@ -6,5 +6,12 @@ class Program
     {
         string[] originalArray = { "Hello", "2", "world", ":-)" };
         int shortStringCount = 0;
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                shortStringCount++;
+            }
+        }
     }
 }
